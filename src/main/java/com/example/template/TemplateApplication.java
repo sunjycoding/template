998adffbc53b42d4098aa1da159abdca12dbeb8e
sunjy <sunjyleisure@gmail.com>
2023-12-20 20:01:31 -0800

    chore: basic code

---
+++ src/main/java/com/example/template/TemplateApplication.java
@@ -0,0 +1,16 @@
+package com.example.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author created by sunjy on 12/20/2023
+ */
+@SpringBootApplication
+public class TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApplication.class, args);
+	}
+
+}
